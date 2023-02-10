024ba5bd9e6e7118b36abce9e5a8fd84db1d1d73
emilyguancamole <eguan2@ugradx.cs.jhu.edu>
2023-02-09 22:54:08 -0500

    passed the given unit tests for c functions

--- 02ass/c_hexfuncs.c
+++ 02ass/c_hexfuncs.c
@@ -8,20 +8,17 @@
 // Read up to 16 bytes from standard input into data_buf.
 // Returns the number of characters read.
 unsigned hex_read(char data_buf[]) {
-    // TODO
-    // 0 is stdin. or do STDIN_FILENO
-    unsigned char_read = 0;
-    while (read(0, data_buf, 1) == 1 && char_read <= 16) {
-        char_read++;
-    }
+    // 0 is stdin
+    int char_read = 0;
+    char_read = read(0, data_buf, 16);
+    
     return char_read;
 }
 
 // Write given nul-terminated string to standard output.
 void hex_write_string(const char s[]) {
-    // TODO
-    while (*s != '\0') { //? check end of string
-        write(1, s, 1); // write 1 char to stdout
+    while (*s != '\0') {
+        write(1, s, 1); 
     } 
     
 }
@@ -30,30 +27,44 @@ void hex_write_string(const char s[]) {
 // hex digits. The formatted offset is stored in sbuf, which must
 // have enough room for a string of length 8.
 void hex_format_offset(unsigned offset, char sbuf[]) {
-    // TODO: implement
-    // cant literate thru offset??? and just put into sbuf
-    long remainder;
-    long quotient = offset;
-    int j = 8;
-    while (quotient != 0 && j >= 0) {
-        remainder = quotient % 16;
-        if (remainder < 10) {
-            sbuf[j--] = 48 + remainder;
-        } else {
-            sbuf[j--] = 55 + remainder;
-        }
-        quotient = quotient / 16;
+    // initialize 8 spaces
+    for (int i = 0; i < 8; i++) {
+        sbuf[i] = 0;
     }
 
+    sbuf[8] = '\0';
+    int i = 7;
+    while (i >= 0) {
+        int n = offset & 0b1111; // 4 bits per digit
+        
+        if (n < 10) {
+            n += 48; // numbers
+        } else {
+            n += 87; // letters, starting from a
+        }
+        sbuf[i] = n; 
+        offset = offset >> 4; // shift by 4 bits to get next digit
+        i--;
+    }
 }
 
 // Format a byte value (in range 0-255) as string consisting of two hex digits. 
 // The string is stored in sbuf.
 void hex_format_byte_as_hex(unsigned char byteval, char sbuf[]) {
-    // TODO
-    int asc = byteval; // ascii number in base what?
-    // convert that to a string??
-    //sbuf[] = //where
+    sbuf[2] = '\0'; // null terminate the string
+    int i = 1;
+    while (i >= 0) {
+        int n = byteval & 0b1111; // 4 bits per digit
+        if (n < 10) {
+            n += 48; // numbers
+        } else {
+            n += 87; // letters, starting from a
+        }
+        sbuf[i] = n; 
+        byteval = byteval >> 4; // shift by 4 bits to get next digit
+        i--;
+    }
+    
 }
 
 // Convert a byte value (in the range 0-255) to a printable character
@@ -62,11 +73,9 @@ void hex_format_byte_as_hex(unsigned char byteval, char sbuf[]) {
 // ASCII code for '.' should be returned.
 char hex_to_printable(unsigned char byteval) { // ? base?
     // TODO
-    int asc = byteval;
-    if (asc > 32 && asc < 127) {
+    if (byteval > 32 && byteval < 127) {
         return byteval;
     } else {
-        asc = '.';
-        return asc;
+        return '.';
     }
 }
