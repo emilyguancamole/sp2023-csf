3af13c4675c36a71573daad0887e7c126256dddf
emilyguancamole <eguan2@ugradx.cs.jhu.edu>
2023-03-07 21:59:30 -0500

    bit shifting to get index and tag

--- 03ass/functions.cpp
+++ 03ass/functions.cpp
@@ -5,7 +5,9 @@ void write_allocate() {
 }
 
 void no_write_allocate() {
-
+// write allocate. if miss, loads.
+// write thru - every time store, need ot store that chunk to memory. storing 4 byte thing
+// write back - won't write thru every store. only syncronize back to main memory when block is evicted
 }
 
 void write_through() {
--- 03ass/main.cpp
+++ 03ass/main.cpp
@@ -2,47 +2,57 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <cmath>
 #include "functions.h"
 
 using std::string;
+using std::cout;
+using std::cin;
 
 int main(int argc, char* argv[]) {
     
     int sets = std::stoi(argv[1]);
     // check to ensure sets is a positive power of 2
     if (!isPowOfTwo(sets)) {
-      // error, number of sets is not a pow of 2
+      return 1;
     }
-    //initialize_sets()
 
-    int blocks = std::stoi(argv[2]);
-    int bytes = std::stoi(argv[3]);
+    int blocks = std::stoi(argv[2]); 
+    int bytes = std::stoi(argv[3]); // block size 
+
     string write_func = argv[4];
     string through_back = argv[5];
     string evictions = argv[6];
 
-    string filename;
-    std::cin >> filename;
-    
-    std::ifstream in(filename); // open the file, store to 'in'
-    if (!in.is_open()) {
-    std::cerr << "Error: could not open input file " << filename << "\n";
-    return 1;
-  }
-    
     string line;
     char command;
-    string address;
-    while (std::getline(in, line)) { // read line by line
+    uint32_t address;
+    uint32_t offset_size = log2(bytes); // ex. passing 16 would give size=4
+    uint32_t index_size = log2(blocks);
+    uint32_t tag_size = 32 - offset_size - index_size; 
+    uint32_t index;
+    uint32_t tag;
+
+    
+    while (std::getline(cin, line)) { // read line by line
         std::istringstream ss(line); // store line into string buffer
         ss >> command;
-        ss >> address;
+        ss >> std::hex >> address; // parse as hexadecimal
         ss.clear(); // last element isn't used
-        // done with reading line
+
+        index = address; // make a copy of address
+        index = index << tag_size >> (tag_size + offset_size); // shift to only get index
+        tag = address;
+        tag = tag >> (index_size + offset_size); // only get tag
+
+        
+        cout << "address: " << address << "\n";
+        cout << "tag: " << tag << "\n";
+        cout << "index: " << index << "\n";
 
         // some type of switch stmt to process command (s or l)
         if (command == 's') {
-
+          
         } 
         else if (command == 'l') {
 
